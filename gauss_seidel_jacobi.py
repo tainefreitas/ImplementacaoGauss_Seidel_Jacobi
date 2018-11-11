b7f0ab6ebf887dc6227c863f6446b76d779eab08
Taíne Freitas <tddfreitas@inf.ufpel.edu.br>
2018-11-11 17:29:20 -0200

    Terminando

--- gauss_seidel_jacobi.py
+++ gauss_seidel_jacobi.py
@@ -1,17 +1,23 @@
 #Trabalho elaborado por: Renan Pacheco e Taine Freitas
+#O trabalho consegue fazer ate x5
 
 from __future__ import division  
 import numpy as np  
 from numpy import linalg 
 import matplotlib.pyplot as plt
-vet_x = []
-vet_y = []
+
+vet_x1=[]
+vet_x2=[]
+vet_x3=[]
+vet_x4=[]
+vet_x5=[]
+
 
 def jacobi(A,b,x0,tol,N):  
     #preliminares
     A = A.astype('double')  
     b = b.astype('double')  
-    x0 = x0.astype('double')  
+    x0 = x0.astype('double') 
  
     n=np.shape(A)[0]  
     x = np.zeros(n)  
@@ -24,13 +30,21 @@ def jacobi(A,b,x0,tol,N):
             x[i] = b[i]  
             for j in np.concatenate((np.arange(0,i),np.arange(i+1,n))):  
                 x[i] -= A[i,j]*x0[j]  
-            x[i] /= A[i,i] 
-            #print(x[i],A[i,i])
-            vet_x.append(x[i])
+            x[i] /= A[i,i]
+            if i == 0:
+                vet_x1.append(x[i])
+            elif i == 1:
+                vet_x2.append(x[i])
+            elif i == 2:
+                vet_x3.append(x[i])
+            elif i == 3:
+                vet_x4.append(x[i])
+            else:
+                vet_x5.append(x[i])
+            
 
         #tolerancia  
         if (np.linalg.norm(x-x0,np.inf) < tol):  
-            #print(x)
             return x  
         #prepara nova iteracao  
         x0 = np.copy(x)  
@@ -53,9 +67,18 @@ def gauss_seidel(A,b,x0,tol,N):
             x[i] = b[i]  
             for j in np.concatenate((np.arange(0,i),np.arange(i+1,n))):  
                 x[i] -= A[i,j]*x[j]  
-            x[i] /= A[i,i]  
-            vet_y.append(x[i])
-          #  print(x[i],A[i,i])  
+            x[i] /= A[i,i]
+            if i == 0:
+                vet_x1.append(x[i])
+            elif i == 1:
+                vet_x2.append(x[i])
+            elif i == 2:
+                vet_x3.append(x[i])
+            elif i == 3:
+                vet_x4.append(x[i])
+            else:
+                vet_x5.append(x[i])
+            
         #tolerancia  
         if (np.linalg.norm(x-x0,np.inf) < tol):  
             #print(x)
@@ -75,15 +98,13 @@ N = 25
 x0 = np.array([5, 4])
 
 x_jacobi = jacobi(A, b, x0, tol, N)
+#
+# plt.plot[vet_x1]
 plt.plot(A)
-plt.title("A")
-plt.show()
-plt.plot(vet_x)
 plt.title("Resultados Jacobi")
 plt.show()
-#Com a funcao exemplo, retorna que deu o nro max de iteracoes, e mesmo alterando, nao 
-#modificou o erro
+
 x_gauss = gauss_seidel(A, b, x0, tol, N)
-plt.plot(vet_y)
-plt.title("Resultados Seidel")
-plt.show()
+#plt.plot(vet_y)
+#plt.title("Resultados Seidel")
+#plt.show()
