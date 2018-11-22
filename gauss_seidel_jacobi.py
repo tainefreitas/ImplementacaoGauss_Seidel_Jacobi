85ac8777b49ad97c2c69fc94e73906f6dadfae53
Taíne Freitas <tddfreitas@inf.ufpel.edu.br>
2018-11-22 12:44:48 -0200

    THE END

--- gauss_seidel_jacobi.py
+++ gauss_seidel_jacobi.py
@@ -6,19 +6,15 @@ import numpy as np
 from numpy import linalg 
 import matplotlib.pyplot as plt
 
-vet_x1=[]
-vet_x2=[]
-vet_x3=[]
-vet_x4=[]
-vet_x5=[]
-
-
 def jacobi(A,b,x0,tol,N):  
     #preliminares
     A = A.astype('double')  
     b = b.astype('double')  
     x0 = x0.astype('double') 
- 
+    vet_x1=[x0[0,0]]
+    vet_x2=[x0[1,0]]
+
+
     n=np.shape(A)[0]  
     x = np.zeros(n)  
     it = 0  
@@ -31,20 +27,25 @@ def jacobi(A,b,x0,tol,N):
             for j in np.concatenate((np.arange(0,i),np.arange(i+1,n))):  
                 x[i] -= A[i,j]*x0[j]  
             x[i] /= A[i,i]
-            if i == 0:
-                vet_x1.append(x[i])
-            elif i == 1:
-                vet_x2.append(x[i])
-            elif i == 2:
-                vet_x3.append(x[i])
-            elif i == 3:
-                vet_x4.append(x[i])
-            else:
-                vet_x5.append(x[i])
-            
-
+            vet_x1.append(x[0])
+            vet_x2.append(x[1])
         #tolerancia  
         if (np.linalg.norm(x-x0,np.inf) < tol):  
+            reta1_x = [0,0]
+            reta2 = [0,0]
+            #Pontos para o traco das retas
+            #Reta 1
+            reta1_x[0] = (b[0] - (-5*A[0, 1]))/A[0,0]
+            reta1_x[1]= (b[1] - (5* A[0, 1]))/A[0,0]
+            reta_y = [-5,5]
+            #Reta 2
+            reta2[0] = (b[0]*A[1,0])/A[1,1]
+            reta2[1] =  (b[1]*A[1,0])/A[1,1]
+
+            plt.plot(reta1_x,reta_y, reta2, reta_y)
+            plt.plot(vet_x1, vet_x2, 'ro')            
+            plt.title("Resultados Jacobi")
+            plt.show()
             return x  
         #prepara nova iteracao  
         x0 = np.copy(x)  
@@ -55,6 +56,8 @@ def gauss_seidel(A,b,x0,tol,N):
     A = A.astype('double')  
     b = b.astype('double')  
     x0 = x0.astype('double')  
+    vet_x1=[x0[0,0]]
+    vet_x2=[x0[1,0]]
 
     n=np.shape(A)[0]  
     x = np.copy(x0)  
@@ -68,42 +71,40 @@ def gauss_seidel(A,b,x0,tol,N):
             for j in np.concatenate((np.arange(0,i),np.arange(i+1,n))):  
                 x[i] -= A[i,j]*x[j]  
             x[i] /= A[i,i]
-            if i == 0:
-                vet_x1.append(x[i])
-            elif i == 1:
-                vet_x2.append(x[i])
-            elif i == 2:
-                vet_x3.append(x[i])
-            elif i == 3:
-                vet_x4.append(x[i])
-            else:
-                vet_x5.append(x[i])
-            
+            vet_x1.append(x[0])
+            vet_x2.append(x[1])
+
         #tolerancia  
-        if (np.linalg.norm(x-x0,np.inf) < tol):  
-            #print(x)
+        if (np.linalg.norm(x-x0,np.inf) < tol): 
+            reta1_x = [0,0]
+            reta2 = [0,0]
+            #Pontos para o traco das retas
+            #Reta 1
+            reta1_x[0] = (b[0] - (-5*A[0, 1]))/A[0,0]
+            reta1_x[1]= (b[1] - (5* A[0, 1]))/A[0,0]
+            reta_y = [-5,5]
+            #Reta 2
+            reta2[0] = (b[0]*A[1,0])/A[1,1]
+            reta2[1] =  (b[1]*A[1,0])/A[1,1]
+
+            plt.plot(reta1_x,reta_y, reta2, reta_y)
+
+            plt.plot(vet_x1, vet_x2, 'ro')
+            plt.title("Resultados Seidel")
+            plt.show()
             return x  
         #prepara nova iteracao  
         x0 = np.copy(x)  
  #   raise NameError('num. max. de iteracoes excedido.')
-A = np.array([[2,5],
-            [3, 1]],
+A = np.array([[1,1],
+            [1, -3]],
             dtype='double')
 
 
-b = np.array([-3,2])
+b = np.array([[3],[-3]])
 tol = 0.001
 N = 25
-x0 = np.array([5, 4])
+x0 = np.array([[0], [0]])
 
 x_jacobi = jacobi(A, b, x0, tol, N)
-plt.plot(A)
-plt.plot(vet_x1, vet_x2)
-plt.title("Resultados Jacobi")
-plt.show()
-
 x_gauss = gauss_seidel(A, b, x0, tol, N)
-plt.plot(A)
-plt.plot(vet_x1, vet_x2)
-plt.title("Resultados Seidel")
-plt.show()
